553ccc7269d93aa05834662e780e5dec0e839e95
yathink3 <52459137+yathink3@users.noreply.github.com>
2019-07-12 14:04:24 +0530

    Delete InnerClass.js

--- InnerClass.js
+++
@@ -1,7 +0,0 @@
-var i = rl.createInterface(process.stdin, process.stdout, null);
-i.question('Write your name: ', function(answer) {
-  console.log('Nice to meet you> ' + answer);
-  i.close();
-  process.stdin.destroy();
-
-});
